a3f027b232f6e786aed69c7e5073bee34f87bcee
Максим Воробьев <maksimvorobev@MacBook-Air.local>
2023-05-16 10:24:55 +0400

    #683430. Поправил замечания по рефакторингу контрл-инпут

--- clustrum-portal/src/clustrum-lib/src/index.ts
+++ clustrum-portal/src/clustrum-lib/src/index.ts
@@ -4,4 +4,4 @@ export {
   InputFilterControl,
   InputFilterControlProps,
 } from './shared/ui/filter-controls/input-filter-control/input-filter-control';
-export { useDebounce } from '../src/shared/lib/hooks/use-debounce/use-debounce';
+export { useDebounce } from './shared/lib/hooks/use-debounce/use-debounce';
