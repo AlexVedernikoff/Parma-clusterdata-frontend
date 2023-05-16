bcc1e5b1f8c2af6ce2176e326bac715e3ed8a744
Максим Воробьев <maksimvorobev@MacBook-Air.local>
2023-05-16 10:14:23 +0400

    #683430. Поправил замечания по рефакторингу контр-инпут

--- clustrum-portal/src/clustrum-lib/src/index.ts
+++ clustrum-portal/src/clustrum-lib/src/index.ts
@@ -1,3 +1,7 @@
 export { default as Wizard } from './modules/legacy-wizard/Wizard';
 export { DashKit, pluginText, pluginTitle, pluginWidget } from './modules/legacy-dashboard/src';
-export { InputFilterControl } from './shared/ui/filter-controls/input-filter-control/input-filter-control';
+export {
+  InputFilterControl,
+  InputFilterControlProps,
+} from './shared/ui/filter-controls/input-filter-control/input-filter-control';
+export { useDebounce } from '../src/shared/lib/hooks/use-debounce/use-debounce';
--- clustrum-portal/src/clustrum-lib/src/shared/ui/filter-controls/input-filter-control/input-filter-control.tsx
+++ clustrum-portal/src/clustrum-lib/src/shared/ui/filter-controls/input-filter-control/input-filter-control.tsx
@@ -1,24 +1,24 @@
-import React, { useState, useEffect, FC } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Input } from 'antd';
-import { useDebounce } from '@clustrum-lib/shared/lib/hooks';
+import { useDebounce } from '../../../lib/hooks/use-debounce/use-debounce';
 import classnames from 'classnames';
 import './input-filter-control.css';
 
-interface IInputFilterControl {
-  label: string;
+export interface InputFilterControlProps {
+  label?: string;
   placeholder: string;
   defaultValue: string;
   onChange(str: string): void;
   className: string;
 }
 
-export const InputFilterControl: FC<IInputFilterControl> = ({
+export function InputFilterControl({
   label,
   placeholder,
   defaultValue,
   onChange,
   className,
-}) => {
+}: InputFilterControlProps): JSX.Element {
   const [value, setValue] = useState(defaultValue);
   const debouncedValue = useDebounce(value, 500);
 
@@ -32,15 +32,16 @@ export const InputFilterControl: FC<IInputFilterControl> = ({
 
   return (
     <div className={classnames('input-filter-control__wrapper', className)}>
-      {label && <span className="input-filter-control__label">{label}:</span>}
-      <Input
-        placeholder={placeholder}
-        value={value}
-        onChange={({ currentTarget }): void => setValue(currentTarget.value)}
-        onPressEnter={(): void => onChange(value)}
-      />
+      <label className="input-filter-control__label">
+        {label}:
+        <Input
+          placeholder={placeholder}
+          value={value}
+          onChange={({ currentTarget }): void => setValue(currentTarget.value)}
+          onPressEnter={(): void => onChange(value)}
+          className="input-filter-control__input"
+        />
+      </label>
     </div>
   );
-};
-
-InputFilterControl.displayName = 'InputFilterControl';
+}
