3ca37e7bc3f7caac0a93759cee0393cf6487c2b5
Zolotko <Zolotko.IA@parmalogica.ru>
2023-06-01 15:27:13 +0500

    #684191 Скрыл кнопки подключения к БД и отрефакторил код

--- clustrum-portal/src/components/Connectors/Connectors.js
+++ clustrum-portal/src/components/Connectors/Connectors.js
@@ -1,89 +1,67 @@
-import React, { useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import block from 'bem-cn-lite';
 import { Button, TextInput } from 'lego-on-react';
 import { Icon } from '@kamatech-data-ui/common/src';
 import { ActionPanel } from '@kamatech-data-ui/clustrum';
 import Utils from './../../helpers/utils';
-import { getConnectorsMap, getConnectorsMap, getFakeEntry } from '../../constants';
+import { getConnectorsMap, getFakeEntry } from '../../constants';
 
 // import './Connectors.scss';
 
 const b = block('connectors');
 
-class Connectors extends React.Component {
-  state = {
-    searchConnectorName: '',
+export function Connectors({ sdk, location: { search } }) {
+  const [searchConnectorName, setSearchConnectorName] = useState();
+
+  const changeSearchConnectorName = searchConnectorName => {
+    setSearchConnectorName(searchConnectorName);
   };
 
-  changeSearchConnectorName = searchConnectorName => {
-    this.setState({
-      searchConnectorName,
-    });
-  };
+  const connectionsList = useMemo(() => {
+    const filteredList = Object.entries(getConnectorsMap()).filter([, title]);
+    return searchConnectorName
+      ? title.toLowerCase().includes(searchConnectorName.toLowerCase().replace(/\s+/g, ''))
+      : filteredList;
+  }, [searchConnectorName]);
 
-  render() {
-    const {
-      sdk,
-      location: { search },
-    } = this.props;
-
-    const { searchConnectorName } = this.state;
-    const connectionsList = useMemo(
-      () =>
-        Object.entries(getConnectorsMap()).filter(([, title]) => {
-          return searchConnectorName
-            ? title.toLowerCase().includes(searchConnectorName.toLowerCase().replace(/\s+/g, ''))
-            : true;
-        }),
-      [searchConnectorName],
-    );
-
-    return (
-      <div className={b()}>
-        <ActionPanel
-          sdk={sdk}
-          entry={getFakeEntry('connection')}
-          // если не опустить пустой div, то в правой части панели будет 0
-          rightItems={[<div key={'fake'} />]}
+  return (
+    <div className={b()}>
+      <ActionPanel sdk={sdk} entry={getFakeEntry('connection')} rightItems={[<div key={'fake'} />]} />
+      <div className={b('panel')}>
+        <TextInput
+          tabIndex={1}
+          cls={b('search-connector-inp')}
+          theme="normal"
+          size="s"
+          view="default"
+          tone="default"
+          text={searchConnectorName}
+          placeholder="Имя коннектора"
+          onChange={e => changeSearchConnectorName(e.target.value)}
+          hasClear
+          focused
         />
-        <div className={b('panel')}>
-          <TextInput
-            tabIndex={1}
-            cls={b('search-connector-inp')}
-            theme="normal"
-            size="s"
-            view="default"
-            tone="default"
-            text={searchConnectorName}
-            placeholder="Имя коннектора"
-            onChange={this.changeSearchConnectorName}
-            hasClear
-            focused
-          />
-        </div>
-
-        <div className={b('list')}>
-          {connectionsList.map(([connector, title]) => {
-            return (
-              <Link key={connector} className={b('link')} to={`/connections/new/${connector}${search}`}>
-                <Button cls={b('connector-btn')} theme="pseudo" size="n" view="default" tone="default">
-                  <Icon
-                    key={connector}
-                    className={b('connector-icon')}
-                    data={Utils.getConnectorIcon({ type: connector })}
-                    width="56"
-                    height="56"
-                  />
-                  <div className={b('connector-title')}>{title}</div>
-                </Button>
-              </Link>
-            );
-          })}
-        </div>
       </div>
-    );
-  }
-}
 
-export default Connectors;
+      <div className={b('list')}>
+        {connectionsList.map(([connector, title]) => {
+          return (
+            <Link key={connector} className={b('link')} to={`/connections/new/${connector}${search}`}>
+              <Button cls={b('connector-btn')} theme="pseudo" size="n" view="default" tone="default">
+                <Icon
+                  key={connector}
+                  className={b('connector-icon')}
+                  data={Utils.getConnectorIcon({ type: connector })}
+                  width="56"
+                  height="56"
+                />
+                <div className={b('connector-title')}>{title}</div>
+              </Button>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
