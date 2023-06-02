500525af96bf999bd3e80377071f14d8119b2f72
Олег Ищенко <ishchenko@parma.ru>
2023-06-02 11:13:22 +0500

    #691027: small fault

--- clustrum-portal/src/clustrum-lib/src/index.ts
+++ clustrum-portal/src/clustrum-lib/src/index.ts
@@ -25,4 +25,4 @@ export {
   RangeDatepickerProps,
 } from './shared/ui/filter-controls/range-datepicker-filter-control/range-datepicker-filter-control';
 export { PickerValue } from './shared/ui/filter-controls/range-datepicker-filter-control/types/picker-value';
-export { TableWidget, TableWidgetProps, valueFormatter } from './shared/ui/widgets';
+export { TableWidget, TableWidgetProps, createCell } from './shared/ui/widgets';
