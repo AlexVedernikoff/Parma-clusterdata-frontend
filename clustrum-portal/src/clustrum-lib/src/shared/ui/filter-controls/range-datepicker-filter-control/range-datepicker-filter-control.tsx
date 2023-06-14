7691b2c466293f67f9dfa314ac75992d17f3b34c
Vdovina <staylimmk@gmail.com>
2023-06-14 22:32:58 +0500

    #697676 датапикер в пределах видимой части страницы

--- clustrum-portal/src/clustrum-lib/src/shared/ui/filter-controls/range-datepicker-filter-control/range-datepicker-filter-control.tsx
+++ clustrum-portal/src/clustrum-lib/src/shared/ui/filter-controls/range-datepicker-filter-control/range-datepicker-filter-control.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import cn from 'classnames';
 import { DatePicker } from 'antd';
 import ruRU from 'antd/locale/ru_RU';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ru';
+import { shouldMoveDropdown } from '../../../lib/utils/should-move-dropdown/should-move-dropdown';
 import { PickerValue } from './types/picker-value';
 import { Range } from './types/range';
 import './range-datepicker-filter-control.css';
@@ -20,6 +21,7 @@ export interface RangeDatepickerProps {
 }
 
 const DEFAULT_DATE_FORMAT = 'YYYY-MM-DD';
+const POPUP_WIDTH = 576;
 
 export function RangeDatepickerFilterControl({
   className,
@@ -32,6 +34,8 @@ export function RangeDatepickerFilterControl({
 }: RangeDatepickerProps): JSX.Element {
   const [dateRange, setDateRange] = useState<Range>(null);
   const [isValid, setIsValid] = useState<boolean>(true);
+  const [shouldMoveCalendar, setShouldMoveCalendar] = useState<boolean>(false);
+  const pickerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const from = dayjs(value?.from);
@@ -43,23 +47,26 @@ export function RangeDatepickerFilterControl({
     }
   }, [isValid, value]);
 
-  const handleChange = useCallback((values: Range): void => {
-    if (values?.[0] && values?.[1]) {
-      setIsValid(true);
-      onChange({
-        from: values[0].format(DEFAULT_DATE_FORMAT),
-        to: values[1].format(DEFAULT_DATE_FORMAT),
-      });
-    } else {
-      setIsValid(false);
-    }
-  }, []);
+  const handleChange = useCallback(
+    (values: Range): void => {
+      if (values?.[0] && values?.[1]) {
+        setIsValid(true);
+        onChange({
+          from: values[0].format(DEFAULT_DATE_FORMAT),
+          to: values[1].format(DEFAULT_DATE_FORMAT),
+        });
+      } else {
+        setIsValid(false);
+      }
+    },
+    [onChange],
+  );
 
   return (
     <div className={cn('range-datepicker-control', className)}>
       <label className="range-datepicker-control__label">
         {`${label}:`}
-        <div className="range-datepicker-control__picker">
+        <div ref={pickerRef} className="range-datepicker-control__picker">
           <RangePicker
             className={cn(!isValid && 'range-datepicker-control__picker--invalid')}
             disabledDate={(current): boolean =>
@@ -69,8 +76,12 @@ export function RangeDatepickerFilterControl({
             format={dateFormat}
             locale={ruRU.DatePicker}
             picker="date"
+            placement={shouldMoveCalendar ? 'bottomRight' : 'bottomLeft'}
             value={dateRange}
             onChange={handleChange}
+            onOpenChange={(): void => {
+              setShouldMoveCalendar(shouldMoveDropdown(pickerRef?.current, POPUP_WIDTH));
+            }}
           />
           {!isValid && (
             <div className="range-datepicker-control__validation-msg">
