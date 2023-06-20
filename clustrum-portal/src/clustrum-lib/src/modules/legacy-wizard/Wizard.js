f73b8512578d78953d98117e4e37120ef8866d1f
SadLion <leva51999@mail.ru>
2023-06-20 08:31:59 +0500

    #697089 fsd

--- clustrum-portal/src/clustrum-lib/src/modules/legacy-wizard/Wizard.js
+++ clustrum-portal/src/clustrum-lib/src/modules/legacy-wizard/Wizard.js
@@ -14,7 +14,7 @@ import Toaster from '@kamatech-data-ui/common/src/components/Toaster';
 
 import SectionDataset from './SectionDataset/SectionDataset';
 import SectionVisualization from './SectionVisualization/SectionVisualization';
-import SectionPreview from '../wizard/section-preview/section-preview';
+import SectionPreview from '../wizard/widgets/section-preview/ui/section-preview';
 
 import DialogNoRights from './components/Dialogs/DialogNoRights';
 
