f170a9c08c6839de6a3d6a3f42f6252752b1829b
Skobeev.VV <skobeev.vv@parma.ru>
2023-08-03 13:06:27 +0300

    привести код к принятым правилам

--- clustrum-portal/src/widgets/base-navigation-modal/ui/base-navigation-modal.tsx
+++ clustrum-portal/src/widgets/base-navigation-modal/ui/base-navigation-modal.tsx
@@ -1,27 +1,20 @@
-import { ReactElement, useEffect, useState } from 'react';
-import React from 'react';
-import { MenuItemType } from 'antd/es/menu/hooks/useItems';
-import { SidePanel } from '../../../features/side-panel/ui/side-panel';
-
+import React, { ReactElement, useEffect } from 'react';
 import { useUnit } from 'effector-react';
-import { Places } from '../../../shared/lib/constants/places';
+import { MenuItemType } from 'antd/es/menu/hooks/useItems';
+import { SidePanel } from '@features/side-panel/ui/side-panel';
+
+import { Places } from '@shared/lib/constants';
 import {
   $pathInFolder,
   $place,
   changePathInFolderEvent,
   changePlaceEvent,
   getNavigationListEvent,
-} from '../../../shared/model/navigation-base-model';
-import { NavigationBase } from '../../../features/navigation-base/ui/navigation-base';
-import './base-navigation-modal.css';
+} from '@shared/model/navigation-base-model';
+import { NavigationBase } from '@features/navigation-base/ui/navigation-base';
+import { NavigationModalComponentProps } from '../types';
+import styles from './base-navigation-modal.module.css';
 
-interface NavigationModalComponentProps {
-  path: string;
-  /**
-   * TODO: убрать данное поле после рефакторинга, нужно для совместимости со старым кодом
-   */
-  sdk: any;
-}
 /* компонент, который будет отображаться в новой модалке (сейчас встроен в старую реализацию) */
 export function BaseNavigationModal(props: NavigationModalComponentProps): ReactElement {
   const { path, sdk } = props;
@@ -60,15 +53,15 @@ export function BaseNavigationModal(props: NavigationModalComponentProps): React
   };
 
   return (
-    <div className="base-navigation-modal">
-      <div className="base-navigation-modal__header">Навигатор объектов</div>
-      <div className="base-navigation-modal__content">
+    <div className={styles['base-navigation-modal']}>
+      <div className={styles['base-navigation-modal__header']}>Навигатор объектов</div>
+      <div className={styles['base-navigation-modal__content']}>
         <SidePanel
           withHeader={false}
           onClickMenuItem={onClickSidePanelMenuItem}
           selectedItem={[place]}
         />
-        <div className="base-navigation-modal__navigation">
+        <div className={styles['base-navigation-modal__navigation']}>
           <NavigationBase
             sdk={sdk}
             isModalView
