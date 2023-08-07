f8961e27fd445ea86587b88ea55e227646bf8c27
Vdovina <staylimmk@gmail.com>
2023-08-07 14:26:24 +0500

    fixed targetItemData type

--- clustrum-portal/src/clustrum-lib/src/shared/ui/drag-n-drop/dnd-state/target-item-data.ts
+++ clustrum-portal/src/clustrum-lib/src/shared/ui/drag-n-drop/dnd-state/target-item-data.ts
@@ -1,10 +1,8 @@
 // Это пока что единственный способ прокинуть в событие end актуальную target item data
-// используется тип any из-за generic type
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-let targetItemData: any | null = null;
+let targetItemData: unknown | null = null;
 
 export const getTargetItemData = <T>(): T | null => {
-  return targetItemData;
+  return targetItemData as T;
 };
 
 export const setTargetItemData = <T>(data: T | null): void => {
