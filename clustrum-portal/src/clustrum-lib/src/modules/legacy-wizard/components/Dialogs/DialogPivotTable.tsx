292462647094b4c502831a81f06d10be63b6f6b0
Petelin Andrey <apetelin6821@gmail.com>
2023-09-01 18:11:44 +0500

    719529 Добавление формулы расчета итогов в модальное окно

--- clustrum-portal/src/clustrum-lib/src/modules/legacy-wizard/components/Dialogs/DialogPivotTable.tsx
+++ clustrum-portal/src/clustrum-lib/src/modules/legacy-wizard/components/Dialogs/DialogPivotTable.tsx
@@ -1,46 +1,64 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import Dialog from '@kamatech-data-ui/common/src/components/Dialog/Dialog';
 import { CastIconsFactory } from '@clustrum-lib/shared/ui/cast-icons-factory';
+import { FormulaEditor } from '@kamatech-data-ui/clustrum/src/components/FieldEditor/components';
 import cloneDeep from 'lodash/cloneDeep';
-import { CastIconType } from '@clustrum-lib/shared/ui/cast-icons-factory/types';
 // TODO разобраться с ошибкой импорта в TS
 // @ts-ignore
 import { CheckBox } from 'lego-on-react';
-
-interface ISubTotalsSettings {
-  enabled?: boolean;
-}
-
-interface IItem {
-  title: string;
-  cast: CastIconType;
-  type: string;
-  subTotalsSettings?: ISubTotalsSettings;
-}
-
-interface DialogPivotTableProps<T> {
-  item: T;
-  callback: (item: IItem) => void;
-}
+import { DialogPivotTableProps, IItem, ISubTotalsSettings } from './types';
 
 export const DialogPivotTable = <T extends IItem>({
   item,
   callback,
+  sdk,
+  fields,
+  aceModeUrl,
 }: DialogPivotTableProps<T>): JSX.Element => {
-  const [enabled, setEnabled] = useState<boolean>(false);
+  const [needSubTotal, setNeedSubTotal] = useState<boolean>(false);
+  const [formula, setFormula] = useState<string>('');
+  const [formulaError, setFormulaError] = useState<{} | null>(null);
+  const { datasetId } = item;
 
   useEffect(() => {
-    setEnabled(item.subTotalsSettings?.enabled ?? false);
+    setNeedSubTotal(item.subTotalsSettings?.needSubTotal ?? false);
+    setFormula(item.subTotalsSettings?.formula || item.formula || '');
   }, [item]);
 
-  const switchEnabled = useCallback(() => setEnabled(prevValue => !prevValue), []);
+  const switchEnabled = useCallback(() => setNeedSubTotal(prevValue => !prevValue), []);
   const onApplay = useCallback(() => {
+    if (formulaError) {
+      console.error(formulaError);
+      return;
+    }
     const newItem = cloneDeep(item);
     const subTotalsSettings: ISubTotalsSettings = newItem.subTotalsSettings ?? {};
-    subTotalsSettings.enabled = enabled;
+    subTotalsSettings.needSubTotal = needSubTotal;
+    subTotalsSettings.formula = formula;
     newItem.subTotalsSettings = subTotalsSettings;
     callback(newItem);
-  }, [enabled, item]);
+  }, [needSubTotal, item, formula]);
+
+  const validateFormula = useCallback(async (formula: string) => {
+    const itemIndex = fields.findIndex((field: T) => field.guid === item.guid);
+    item.formula = formula;
+    fields[itemIndex] = item;
+
+    try {
+      const response = await sdk.bi.validateFormula({
+        dataSetId: datasetId,
+        version: 'draft',
+        resultSchema: fields,
+        field: item,
+      });
+      if (response) {
+        setFormula(formula);
+        setFormulaError(null);
+      }
+    } catch (error) {
+      setFormulaError(error as {});
+    }
+  }, []);
 
   return (
     <Dialog visible onClose={callback}>
@@ -50,18 +68,30 @@ export const DialogPivotTable = <T extends IItem>({
           insertBefore={<CastIconsFactory iconType={item.cast} />}
         />
         <Dialog.Body>
-          <div className="subcontainer">
-            <div className="subitem">
-              <CheckBox
-                theme="normal"
-                size="n"
-                view="default"
-                tone="default"
-                checked={enabled}
-                text="Подытоги"
-                onChange={switchEnabled}
-              />
+          <div className="dialog-pivot-table-container">
+            <div className="subcontainer">
+              <div className="subitem">
+                <CheckBox
+                  theme="normal"
+                  size="n"
+                  view="default"
+                  tone="default"
+                  checked={needSubTotal}
+                  text="Подытоги"
+                  onChange={switchEnabled}
+                />
+              </div>
             </div>
+            <FormulaEditor
+              sdk={sdk}
+              isNewField={false}
+              datasetId={datasetId}
+              formula={formula}
+              fields={fields}
+              isVisibleFunctionManual={false}
+              aceModeUrl={aceModeUrl}
+              onChange={validateFormula}
+            />
           </div>
         </Dialog.Body>
         <Dialog.Footer
