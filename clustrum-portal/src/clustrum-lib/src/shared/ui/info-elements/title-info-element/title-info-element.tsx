0c015a93b42378bbb0a0cda4a242e84caa442041
AlexVedernikoff <templar_sc@mail.ru>
2023-09-05 12:27:15 +0300

    bug/vedernikov/716466-Dimension-is-not-applied-to-the-headers-on-the-panel

--- clustrum-portal/src/clustrum-lib/src/shared/ui/info-elements/title-info-element/title-info-element.tsx
+++ clustrum-portal/src/clustrum-lib/src/shared/ui/info-elements/title-info-element/title-info-element.tsx
@@ -5,14 +5,14 @@ import './title-info-element.css';
 
 export const TitleInfoElement = React.forwardRef<HTMLDivElement, TitleInfoElementProps>(
   (
-    { data: { text = '', showInTOC = false } }: TitleInfoElementProps,
+    { data: { text = '', showInTOC = false, size } }: TitleInfoElementProps,
     ref: React.Ref<HTMLDivElement>,
   ): JSX.Element => {
     const id = showInTOC && text ? encodeURIComponent(text) : undefined;
 
     return (
       <div ref={ref} id={id} className="title-info-element">
-        <div className={'title-info-element__title'}>{text}</div>
+        <div className={`title-info-element__title_${size}`}>{text}</div>
       </div>
     );
   },
