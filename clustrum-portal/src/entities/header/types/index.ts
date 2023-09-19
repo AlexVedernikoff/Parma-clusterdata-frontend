38050189d32419b2321a4cfb895259c961a6121b
DENIS LUKIN <nash1q@yandex.ru>
2023-09-19 11:25:56 +0300

    Добавлены переходы по ссылкам из урла

--- clustrum-portal/src/entities/header/types/index.ts
+++ clustrum-portal/src/entities/header/types/index.ts
@@ -1,2 +1,3 @@
 export { BreadcrumbItem } from './breadcrumb-item';
 export { HeaderProps } from './header-props';
+export { EntryByPathRequestParams } from './entry-by-path-request-params';
--- clustrum-portal/src/entities/header/ui/header.tsx
+++ clustrum-portal/src/entities/header/ui/header.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Breadcrumb, Tooltip } from 'antd';
 import { HeaderProps } from '../types';
 
 import './header.css';
+import { Link, generatePath, useHistory } from 'react-router-dom';
+import { ItemType } from 'antd/es/breadcrumb/Breadcrumb';
+import { useUnit } from 'effector-react';
+import { MAP_PLACE_TO_PATH_IN_FOLDER } from '@entities/navigation-base/lib/constants';
+import { Places, ROUTES } from '@shared/config/routing';
+import { $pathInFolder, $place } from '@entities/navigation-base';
+import { getEntryDataByPathApi } from '../api/get-entry-by-path-api';
 
 export function Header({
   leftSideContent,
@@ -10,11 +17,44 @@ export function Header({
   path,
   title,
 }: HeaderProps): JSX.Element {
+  const history = useHistory();
+  const [place, pathInFolder] = useUnit([$place, $pathInFolder]);
+
+  function breadCrumbItemRender(route: any, params: null, items: ItemType[]): ReactNode {
+    const last = items.indexOf(route) === items.length - 1;
+    const getEntryPath = async (): Promise<void> => {
+      const targetUrlKey = route.title + ROUTES.root;
+
+      if (pathInFolder.includes(targetUrlKey)) {
+        const newUrlPath = pathInFolder.substring(
+          0,
+          pathInFolder.indexOf(targetUrlKey) + targetUrlKey.length,
+        );
+        const entryData = await getEntryDataByPathApi({ key: newUrlPath });
+        history.push(
+          generatePath(MAP_PLACE_TO_PATH_IN_FOLDER[place as Places], {
+            id: entryData.entryId,
+          }),
+        );
+      } else {
+        history.push(place === Places.Root ? ROUTES.root : ROUTES.root + place);
+      }
+    };
+
+    return last ? (
+      <span>{route.title}</span>
+    ) : (
+      <Link onClick={getEntryPath} to={'#'}>
+        {route.title}
+      </Link>
+    );
+  }
+
   return (
     <header className="header-wrapper">
       <div className="header-wrapper__breadcrumb">
         <div className="header-wrapper__breadcrumb-patch">
-          <Breadcrumb items={path} />
+          <Breadcrumb itemRender={breadCrumbItemRender} items={path} />
         </div>
       </div>
       <div className="header-wrapper__content">
