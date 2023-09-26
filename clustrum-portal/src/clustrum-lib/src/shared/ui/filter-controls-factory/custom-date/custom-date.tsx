eb30a05ecbb036ae95c4969a7fdddaf4c9b234c6
DENIS LUKIN <nash1q@yandex.ru>
2023-09-26 09:37:09 +0300

    Рефакторинг

--- clustrum-portal/src/clustrum-lib/src/shared/ui/filter-controls-factory/custom-date/custom-date.tsx
+++ clustrum-portal/src/clustrum-lib/src/shared/ui/filter-controls-factory/custom-date/custom-date.tsx
@@ -1,9 +1,11 @@
 import { Dayjs } from 'dayjs';
 import React from 'react';
 
+const ANT_RANGE_DEFAULT_TD_WIDTH = 35.99;
+
 export const renderCustomDate = (format: string): ((current: Dayjs) => JSX.Element) => {
   const newCell = (current: Dayjs): JSX.Element => {
-    const styles = { width: 35.99 };
+    const styles = { width: ANT_RANGE_DEFAULT_TD_WIDTH };
     return (
       <td title={current.format(format)} className="ant-picker-cell" style={styles}>
         {current.date()}
