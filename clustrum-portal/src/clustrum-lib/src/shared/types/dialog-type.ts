d10719c8cb20960886e50d7b977d62944013e2ad
Олег Ищенко <ishchenko@parma.ru>
2023-09-29 12:36:49 +0500

    #728214: CR

--- clustrum-portal/src/clustrum-lib/src/shared/types/dialog-type.ts
+++ clustrum-portal/src/clustrum-lib/src/shared/types/dialog-type.ts
@@ -5,5 +5,5 @@ export enum DialogType {
   Control = 'control',
   Tabs = 'tabs',
   Connections = 'connections',
-  DashboardSettings = 'dashboard-settings',
+  DashboardSettings = 'dashboardSettings',
 }
