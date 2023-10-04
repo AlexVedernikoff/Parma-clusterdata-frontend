9cc05a61af1fef115903b29faf65a17e9b951c9a
Ageev V.A <Ageev.VA@parma.ru>
2023-10-04 12:39:43 +0500

    #731633: Корректное название файла дефолтных стилей

--- clustrum-portal/src/shared/app-settings/lib/constants/index.ts
+++ clustrum-portal/src/shared/app-settings/lib/constants/index.ts
@@ -1,2 +1,2 @@
 export { DEFAULT_ENV_VARIABLES } from './default-env-variables';
-export { DEFAULT_FONT_FAMILY } from './defautl-theme-variables';
+export { DEFAULT_FONT_FAMILY } from './default-theme-variables';
