fc860562d03a404f7f2cfa7581610d0542affd35
Ageev V.A <Ageev.VA@parma.ru>
2023-10-09 15:12:32 +0500

    #720188: Добавил импорт

--- clustrum-portal/src/components/DataSource/VerificationModal.js
+++ clustrum-portal/src/components/DataSource/VerificationModal.js
@@ -9,6 +9,7 @@ import { ConnectionInfo, DataSourceButton, Status } from './components';
 import ErrorView from '../ErrorView/ErrorView';
 import { Stage } from './Stage';
 import { NotificationType } from '@clustrum-lib/shared/lib/notification/types';
+import { NotificationContext } from '@lib-shared/lib/notification';
 
 const b = block('data-source');
 
